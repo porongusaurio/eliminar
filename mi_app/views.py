3c695655dce675830a74133594008bbc6b657d38
porongusario <counterjosefoo@gmail.com>
2025-12-01 19:08:03 -0300

    Agrego templates y rutas index/inicio

--- mi_app/views.py
+++ mi_app/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def inicio(request):
     return HttpResponse("Hola Django!")
+
+def index(request):
+   return render(request, 'mi_app/index.html')
