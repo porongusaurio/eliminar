3b9dfbc59ee3b64e4c59e933a725e5617b22fc9c
porongusario <counterjosefoo@gmail.com>
2025-12-01 18:40:41 -0300

    mensaje descriptivo del cambio

---
+++ mi_proyecto/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('mi_app.urls')),  # ← conectar la app
+]
