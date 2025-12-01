61b169db9af5f7120bc0f50508243126bd90b26a
porongusario <counterjosefoo@gmail.com>
2025-12-01 19:24:54 -0300

    Agregando about.html

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.inicio),
     path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -5,4 +5,7 @@ def inicio(request):
     return HttpResponse("Hola Django!")
 
 def index(request):
-   return render(request, 'mi_app/index.html')
+    return render(request, 'mi_app/index.html')
+
+def about(request):
+    return render(request, 'mi_app/about.html')
